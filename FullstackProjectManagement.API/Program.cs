b69ba124b215f5df80085620846981beba8552db
GeeksMinister <nael_sps@hotmail.com>
2022-08-20 19:31:48 +0200

    Using SwaggerUI in Azure

--- FullstackProjectManagement.API/Program.cs
+++ FullstackProjectManagement.API/Program.cs
@@ -46,11 +46,11 @@ builder.Services.AddCors(policy =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
