16fc472044c8cf927926e8aa10b8c60027edfb24
GeeksMinister <nael_sps@hotmail.com>
2022-08-26 00:05:51 +0200

    Replaced NewtonSoft.Json with Text.Json.Serialization. Changed colors and visibility for 'GameOver' in Simon.

--- FullstackProjectManagement.WASM/Authentication/AuthenticationService.cs
+++ FullstackProjectManagement.WASM/Authentication/AuthenticationService.cs
@@ -1,50 +1,47 @@
-﻿using Newtonsoft.Json;
-using System.Net.Http.Json;
-
-namespace FullstackProjectManagement.WASM.Authentication;
+﻿namespace FullstackProjectManagement.WASM.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
-	private readonly AuthenticationStateProvider _stateProvider;
-	private readonly IConfiguration _config;
-	private readonly ILocalStorageService _localStorage;
-	private readonly HttpClient _client;
+    private readonly AuthenticationStateProvider _stateProvider;
+    private readonly IConfiguration _config;
+    private readonly ILocalStorageService _localStorage;
+    private readonly HttpClient _client;
 
-	public AuthenticationService(AuthenticationStateProvider stateProvider,
-							  IConfiguration config,
-							  ILocalStorageService localStorage,
-							  HttpClient client)
-	{
-		_stateProvider = stateProvider;
-		_config = config;
-		_localStorage = localStorage;
-		_client = client;
-	}
+    public AuthenticationService(AuthenticationStateProvider stateProvider,
+                              IConfiguration config,
+                              ILocalStorageService localStorage,
+                              HttpClient client)
+    {
+        _stateProvider = stateProvider;
+        _config = config;
+        _localStorage = localStorage;
+        _client = client;
+    }
 
-	public async Task<AuthenticatedUserModel?> Login(AuthenticationUserModel user)
-	{
-		string location = _config["ApiLocation"]! + "/Login";
-		var authResult = await _client.PostAsJsonAsync(location, user);
-		var resultContent = await authResult.Content.ReadAsStringAsync();
+    public async Task<AuthenticatedUserModel?> Login(AuthenticationUserModel user)
+    {
+        string location = _config["ApiLocation"]! + "/Login";
+        var authResult = await _client.PostAsJsonAsync(location, user);
+        var resultContent = await authResult.Content.ReadAsStringAsync();
 
-		if (authResult.IsSuccessStatusCode == false)
-		{
-			return null;
-		}
+        if (authResult.IsSuccessStatusCode == false)
+        {
+            return null;
+        }
 
-		var authenticatedUser = JsonConvert.DeserializeObject<AuthenticatedUserModel>(resultContent);
-		await _localStorage.SetItemAsync(_config["authTokenStorageKey"], authenticatedUser!.Access_Token);
-		_client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authenticatedUser.Access_Token);
-		((AuthStateProvider)_stateProvider).NotifyUserAuthentication(authenticatedUser.Access_Token!);
+        var authenticatedUser = JsonSerializer.Deserialize<AuthenticatedUserModel>(resultContent);
+        await _localStorage.SetItemAsync(_config["authTokenStorageKey"], authenticatedUser!.Access_Token);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", authenticatedUser.Access_Token);
+        ((AuthStateProvider)_stateProvider).NotifyUserAuthentication(authenticatedUser.Access_Token!);
 
-		return authenticatedUser;
-	}
+        return authenticatedUser;
+    }
 
-	public async Task LogOut()
-	{
-		await _localStorage.RemoveItemAsync(_config["authTokenStorageKey"]);
-		_client.DefaultRequestHeaders.Authorization = null;
-		((AuthStateProvider)_stateProvider).NotifyUserLogout();
-	}
+    public async Task LogOut()
+    {
+        await _localStorage.RemoveItemAsync(_config["authTokenStorageKey"]);
+        _client.DefaultRequestHeaders.Authorization = null;
+        ((AuthStateProvider)_stateProvider).NotifyUserLogout();
+    }
 }
 
--- FullstackProjectManagement.WASM/Usings.cs
+++ FullstackProjectManagement.WASM/Usings.cs
@@ -1,8 +1,8 @@
 ﻿global using Refit;
 global using System;
 global using System.Linq;
-global using System.Text;
 global using System.Text.Json;
+global using System.Net.Http.Json;
 global using Blazored.LocalStorage;
 global using System.Security.Claims;
 global using System.Threading.Tasks;
@@ -10,7 +10,6 @@ global using System.Net.Http.Headers;
 global using System.Collections.Generic;
 global using Microsoft.AspNetCore.Components.Authorization;
 global using FullstackProjectManagement.WASM.Authentication;
-
 
 global using DataAccessLibrary.DTOs;
 global using DataAccessLibrary.Models;
